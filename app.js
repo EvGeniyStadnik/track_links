7a80d4fce320b4630de03766c4ac1ee6205f4f46
Evgeniy Stadnik <evgeniy.stadnik@gmail.com>
2020-04-26 21:40:47 +0300

    add auth routes

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const config = require('config');
 const mongoose = require('mongoose');
+const authRoutes = require('./routes/auth.routes');
 
 const app = express();
+
+app.use('api/auth', authRoutes)
+
 const PORT = config.get('port') || 5000;
 
 async function start() {

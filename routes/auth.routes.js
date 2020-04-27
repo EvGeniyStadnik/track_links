ea90c0d93ad7f24c8e6aaec73d6b82729da83e56
Evgeniy Stadnik <evgeniy.stadnik@gmail.com>
2020-04-27 15:36:53 +0300

    Add user model and save registered user in DB

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,11 +1,24 @@
 const { Router } = require('express');
+const brypt = require('bcrypt');
+const User = require('../models/User');
 const router = Router();
 
 //'api/auth/register'
 router.post('/register', async(req, res) => {
   try{
-
     const {email, password} = req.body;
+
+    const candidate = await User.findOne({email});
+    if(candidate){
+      return res.status(400).json({message: 'Такой пользователь уже существует'})
+    }
+
+    const hashedPassword = await brypt.hash(password, 12);
+
+    const user = new User({email, password: hashedPassword});
+    await user.save();
+
+    res.status(201).json({message: 'Пользователь создан'})
 
   }catch(e){
     res.status(500).json({message: 'Что то пошло не так, попробуйте снова'})

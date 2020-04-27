7e4351401848dc33b913ef0904a8c31efdf4290c
Evgeniy Stadnik <evgeniy.stadnik@gmail.com>
2020-04-27 18:53:16 +0300

    Add login and validate user

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,14 +1,18 @@
 const { Router } = require('express');
-const brypt = require('bcrypt');
+const bcrypt = require('bcrypt');
 const {check, validationResult} = require('express-validator');
+const jwt = require('jsonwebtoken');
+
+const config = require('config');
 const User = require('../models/User');
+
 const router = Router();
 
 //'api/auth/register'
 router.post(
   '/register',
   [
-    check('email', 'Invalid email').isEmail,
+    check('email', 'Invalid email').isEmail(),
     check('password', 'Length of password should be more then 6 chars').isLength({min: 6})
   ],
   async(req, res) => {
@@ -28,10 +32,11 @@ router.post(
       return res.status(400).json({message: 'Такой пользователь уже существует'})
     }
 
-    const hashedPassword = await brypt.hash(password, 12);
+    const hashedPassword = await bcrypt.hash(password, 12);
     const user = new User({email, password: hashedPassword});
     await user.save();
 
+    // 201 - создание чего либо
     res.status(201).json({message: 'Пользователь создан'})
 
   }catch(e){
@@ -40,12 +45,45 @@ router.post(
 })
 
 //'api/auth/login'
-router.post('/login', async(req, res) => {
-  try{
+router.post('/login',
+  [
+    check('email', 'Email not correct').normalizeEmail().isEmail(),
+    check('password', 'Password should exists').exists()
+  ],
+  async(req, res) => {
 
-  }catch(e){
-    res.status(500).json({message: 'Что то пошло не так, попробуйте снова'})
-  }
+    try{
+      const errors = validationResult(req);
+      if(!errors.isEmpty()){
+        res.status(400).json({
+          errors: errors.array(),
+          message: 'Incorrect login or password'
+        })
+      }
+
+      const {email, password} = req.body;
+
+      const user = await User.findOne({email})
+      if(!user){
+        res.status(400).json({message: 'User not exists'})
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password)
+      if(!isMatch){
+        res.status(400).json({message: 'Password is incorrect'})
+      }
+
+      const token = jwt.sign(
+        {userId: user.id},
+         config.get('jwtSecret'),
+        {expiresIn: '1h'}
+        )
+
+      res.json({token, userId: user.id})
+
+    }catch(e){
+      res.status(500).json({message: 'Что то пошло не так, попробуйте снова'})
+    }
 })
 
 module.exports = router;

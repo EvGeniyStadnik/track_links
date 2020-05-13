9eaf09f8eaa50c9a5f46a65ba7b383bb36914cbc
Evgeniy Stadnik <evgeniy.stadnik@gmail.com>
2020-05-13 21:24:11 +0300

    Add server env setup

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const config = require('config');
 const mongoose = require('mongoose');
+const path = require('path');
 const authRoutes = require('./routes/auth.routes');
 const linkRoutes = require('./routes/link.routes');
 const redirectRoutes = require('./routes/redirect.routers');
@@ -11,6 +12,14 @@ app.use(express.json({extended: true}));
 app.use('/api/auth', authRoutes)
 app.use('/api/link', linkRoutes)
 app.use('/t', redirectRoutes)
+
+if(process.env.NODE_ENV === 'production'){
+  app.use('/', express.static(path.join(__dirname, 'client', 'build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 const PORT = config.get('port') || 5000;
 
